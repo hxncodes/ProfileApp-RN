eda22fc3ba8e71fc0a9604556c5faa820bc8ab6f
Hasnat Rana <hxncodes@gmail.com>
2023-02-24 09:56:56 +0500

    Profile App in React-Native initialized

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+import FlatCard from './Components/FlatCard'
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <FlatCard />
+    </SafeAreaView>
+  )
+}
+
+export default App
---
+++ Components/FlatCard.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const FlatCard = () => {
+  return (
+    <View>
+      <Text>FlatCard</Text>
+    </View>
+  )
+}
+
+export default FlatCard
+
+const styles = StyleSheet.create({})
